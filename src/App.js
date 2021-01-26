b1e0a21bf577b0b2aa58a568bd23e8d8b9a09f38
Sergio <64083738+SergiusGG@users.noreply.github.com>
2021-01-26 17:50:52 +0100

    Subida archivos ejercicio react-template
    Subida de archivos js y json del ejercicio react-template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
-export default () => {
+import React from "react";
+import { render } from "react-dom";
+import Book from "./book";
+import {Libros} from "./frases.json";
 
-    return (
-        <div>
-            hello World!
-        </div>
-    );
+
+
+// function App() {
+//     return (
+//        <Book/>
+//     )
+// }
+
+// export default App
+
+
+export default () => { 
+
+    return ( 
+
+        <div> 
+
+            <Book name="Viatge a la lluna"/>
+
+        </div> 
+
+      );
+
 };
+
+
+
+
+
+
+
+
+
+
+
+// export default () => { 
+
+// render() 
+// {
+
+//     return <Book/>
+
+// }
+// };
+
+// export default () => {
+
+//     return (
+//         <div>
+//             hello World!
+//         </div>
+//     );
+// };
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+// import Book from 'book';
+
+// class App extends Component{
+// // export default () => { 
+
+// render() 
+// {
+
+//     return <book/>;
+
+// }
+// };
+// export default App;
---
+++ src/book.js
@@ -0,0 +1,62 @@
+
+import React from "react";
+
+// function Book() {
+//     return (
+        
+//            <h1>Viatge a la lluna</h1>
+        
+//     )
+// }
+
+// export default Book
+
+export default (props) => {
+
+    return (
+        
+        <h1>{props.name}</h1>
+     
+ )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import ReactDOM from 'react-dom';
+
+// export default () => {
+
+//     return ( 
+//             <h1>Viaje a la luna</h1> 
+
+//       );
+
+// }
+
+// export default () => {
+
+//     <h1>Viaje a la luna</h1> 
+
+// }
+// const Book =()=>{
+//     // export default () => {
+    
+//         return ( 
+//                 <h1>Viaje a la luna</h1> 
+    
+//           );
+    
+//     }
+    
+//     export default Book;
